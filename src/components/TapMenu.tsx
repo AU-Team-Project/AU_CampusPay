76cdf1939228e9364b461b09b616314de3ba9286
heevic <dbgmltmd0317@gmail.com>
2023-09-19 01:26:31 +0900

    - 상단 메뉴 컴포넌트 - 상단 메뉴 컴포넌트 디자인 - 역할별 메뉴 표시 기능 - 일자별 메뉴 표시 기능

--- src/components/TapMenu.tsx
+++ src/components/TapMenu.tsx
@@ -1,16 +1,44 @@
 'use client'
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
+import PaymentBtn from "@/app/payment/PaymentBtn";
 
 const TapMenu = () => {
     const [activeTab, setActiveTab] = useState('교직원');
     /* TODO - 임시 데이터 추가시 타입 재정의 예정 */
-    const [text, setText] = useState<any>('데이터 로딩중...');
+    const [text, setText] = useState<any[]>([]);
+    const [todayDate, setTodayDate] = useState<string>('데이터 로딩중...'); // 초기에 "데이터 로딩중..."으로 설정
 
     useEffect(() => {
         fetch('/api/cooks')
             .then((res) => res.json())
-            .then((data) => setText(data.data));
-    }, []);
+            .then((data) => {
+                const today = new Date();
+                const y = today.getFullYear();
+                const m = today.getMonth() + 1;
+                const d = today.getDate();
+
+                const todayMenus = data.data.filter((menu) => {
+                    return menu.date.y === y && menu.date.m === m && menu.date.d === d && menu.role === activeTab;
+                });
+
+                setText(todayMenus.length > 0 ? todayMenus.map((menu) => menu.menu.join(", ")) : ['오늘의 메뉴가 없습니다.']);
+                setTodayDate(`${m}월 ${d}일`);
+            });
+    }, [activeTab]);
+    const selectedItem = 21;
+
+    const renderMenus = () => {
+        return text.map((menuText, index) => (
+            <div className="w-1/3 min-h-fit max-h-100 p-10 border-solid border-2 border-black-500 ml-auto mr-auto mb-5 rounded" key={index}>
+                <div className="w-fit px-5 m-auto text-center font-semibold -translate-y-12 border-solid border-2 border-black-500 rounded-lg bg-stone-200">{todayDate}</div>
+                {String(menuText).split(",").map((item, itemIndex) => (
+                    <div className="p-0.5 text-center font-medium" key={itemIndex}>
+                        {item}
+                    </div>
+                ))}
+            </div>
+        ));
+    };
 
     return (
         <div className="m-5 mb-0 col-span-2 bg-white">
@@ -31,10 +59,8 @@ const TapMenu = () => {
                 >기숙사
                 </button>
             </div>
-            <div>
-                {activeTab === '교직원' && <div className='p-5 pt-0'>{text[0].menu}</div>}
-                {activeTab === '학생' && <div className='p-5 pt-0'>학생 메뉴</div>}
-                {activeTab === '기숙사' && <div className='p-5 pt-0'>기숙사 메뉴</div>}
+            <div className='flex m-auto'>
+                {renderMenus()}
             </div>
         </div>
     );
