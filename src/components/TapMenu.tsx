6a245c456aedb28141893bfe8abbb18f46736335
heevic <dbgmltmd0317@gmail.com>
2023-09-19 22:03:10 +0900

    feat: TabMenuComponent
    TabMenuComponent 수정하기

--- src/components/TapMenu.tsx
+++ src/components/TapMenu.tsx
@@ -1,12 +1,12 @@
 'use client'
 import React, { useEffect, useState } from 'react';
-import PaymentBtn from "@/app/payment/PaymentBtn";
+import TabMenuPaymentBtn from '@/components/ui/TabMenuPaymentBtn'
 
 const TapMenu = () => {
     const [activeTab, setActiveTab] = useState('교직원');
     /* TODO - 임시 데이터 추가시 타입 재정의 예정 */
     const [text, setText] = useState<any[]>([]);
-    const [todayDate, setTodayDate] = useState<string>('데이터 로딩중...'); // 초기에 "데이터 로딩중..."으로 설정
+    const [todayDate, setTodayDate] = useState<string>('데이터 로딩중...');
 
     useEffect(() => {
         fetch('/api/cooks')
@@ -17,27 +17,38 @@ const TapMenu = () => {
                 const m = today.getMonth() + 1;
                 const d = today.getDate();
 
-                const todayMenus = data.data.filter((menu) => {
+                const todayMenus = data.data.filter((menu: any) => {
                     return menu.date.y === y && menu.date.m === m && menu.date.d === d && menu.role === activeTab;
                 });
 
-                setText(todayMenus.length > 0 ? todayMenus.map((menu) => menu.menu.join(", ")) : ['오늘의 메뉴가 없습니다.']);
+                setText(todayMenus.length > 0 ? todayMenus.map((menu: any) => menu.menu.join(", ")) : ['오늘의 메뉴가 없습니다.']);
                 setTodayDate(`${m}월 ${d}일`);
             });
     }, [activeTab]);
-    const selectedItem = 21;
 
     const renderMenus = () => {
-        return text.map((menuText, index) => (
-            <div className="w-1/3 min-h-fit max-h-100 p-10 border-solid border-2 border-black-500 ml-auto mr-auto mb-5 rounded" key={index}>
-                <div className="w-fit px-5 m-auto text-center font-semibold -translate-y-12 border-solid border-2 border-black-500 rounded-lg bg-stone-200">{todayDate}</div>
-                {String(menuText).split(",").map((item, itemIndex) => (
-                    <div className="p-0.5 text-center font-medium" key={itemIndex}>
-                        {item}
+        return text.map((menuText, index) => {
+            const items = String(menuText).split(","); // 아이템들을 배열로 분할
+            return (
+                <div className="w-11/12 min-h-[125px] p-5 border-solid border-2 border-black-500 ml-auto mx-auto mb-5 rounded" key={index}>
+                    <div className="w-fit px-5 m-auto text-center font-semibold -translate-y-9 border-solid border-2 border-black-500 rounded-lg bg-stone-200">{todayDate}</div>
+                    <div className='min-h-full text-center'>
+                        {items.map((item, itemIndex) => (
+                            <div className="p-0.5 min text-center font-medium text-[18px]" key={itemIndex}>
+                                {item}
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
-        ));
+                    {items[0] !== '오늘의 메뉴가 없습니다.' && (
+                        <div className='flex -translate-y-20'>
+                            <TabMenuPaymentBtn props={{ name: items[0], amount: menuText.price }} />
+                        </div>
+                    )}
+
+
+                </div>
+            );
+        });
     };
 
     return (
@@ -59,9 +70,10 @@ const TapMenu = () => {
                 >기숙사
                 </button>
             </div>
-            <div className='flex m-auto'>
+            <div className="grid grid-cols-3 gap-4 m-auto mt-10">
                 {renderMenus()}
             </div>
+
         </div>
     );
 };
