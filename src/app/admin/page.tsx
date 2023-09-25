175496c8789c02fa2462be73578b203837104fee
heevic <dbgmltmd0317@gmail.com>
2023-09-25 16:06:43 +0900

    feat: Create Admin Page
    서버 사이드로 작동하는 app/admin 페이지 작성
    admin 페이지에 로그인 세션이 있는지, role이 admin인지 확인 하는 접근 제어 기능 개발
    Components/adminNav.tsx 컴포넌트 작성
    admin Nav에 대시보드, 통계(사용자 통계, 판매 통계), 사용자 관리, 시스템 모니터링(서버 상태 모니터링, 로그 파일 관리), 컨텐츠 관리(게시물 관리, 카테고리 관리, 메뉴 관리), 설정(시스템 설정, 보안 설정) 메뉴 스타일링

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import VerticalNav from "@/components/adminNav";
+import {getServerSession} from "next-auth";
+import {options} from "@/app/api/auth/[...nextauth]/options";
 
-const AdminPage = () => {
+const AdminPage = async () => {
+    const session = await getServerSession(options);
+    if(!session?.user.role || !session) {
+        window.location.href='/';
+    }
+    console.log(session)
+
     return (
-        <div>
-            
+        <div className="flex">
+            <div className="w-[400px]">
+                <VerticalNav />
+            </div>
+
+            <main className="w-screen grid-cols-3 grid-rows-2">
+                <div className="w-1/3">
+                    하루 판매 수dddd
+                </div>
+                <div className="w-1/3">
+                    하루 거래 액
+                </div>
+                <div className="w-1/3">
+                    하루 취소 액
+                </div>
+            </main>
         </div>
     );
 };
