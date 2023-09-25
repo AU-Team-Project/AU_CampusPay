1385218237423427775efb0fdecdac54bc2d9ae4
japark <wkd5878@gmail.com>
2023-09-25 19:33:58 +0900

    Feat - 게시글 삭제 API 구현
    - 제공된 _id를 기반으로 게시글을 삭제하는 API를 개발
    - Postman을 사용하여 게시물(공지사항) 삭제 기능을 성공적으로 테스트
    
    Test
    - Postman 요청 : http://127.0.0.1:3000/api/admin/delete
    - Body
    {
        "_id": "65011739eb75cc728772c75b",
        "title": "test",
        "content": "hello"
    }
    - Result
    {
        "success": true,
        "statue": 200,
        "message": "게시글이 삭제되었습니다."
    }

--- src/app/api/confirmation/[slug]/route.ts
+++ src/app/api/confirmation/[slug]/route.ts
@@ -1,8 +1,12 @@
-import {NextRequest, NextResponse} from "next/server";
+import {NextResponse} from "next/server";
 import {connectDB} from "@/app/api/db/mongoDb";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
     const db = (await connectDB).db(process.env.MONGODB_NAME as string);
     const result = await db.collection(process.env.MONGODB_PAYMENT as string).find().toArray();
-    return NextResponse.json({ data: result, pretty: true })
+    return NextResponse.json({
+        data: result,
+        status: 200,
+        pretty: true
+    })
 };
