583b2db9467362b255e184985489a13845195e20
heevic <dbgmltmd0317@gmail.com>
2023-09-28 21:25:29 +0900

    feat: Login Page Design
    public 디렉토리에 Logo_AU_CampusPay_Blue.svg 파일 추가,
    EmailIcon.tsx(src\components\ui\icons\EmailIcon.tsx) 아이콘 생성
    PasswordIcon.tsx(src\components\ui\icons\PasswordIcon.tsx)
    tailwind.config.js에 기존 xmd 제거 및 sm,md,lg의 width 값 정의
    Page.tsx(src\app\(auth)\login\page.tsx)에 반응형 스타일 추가,
    인덱스 페이지로 이동하는 로고 이미지 추가,
    '로그인' 텍스트의 위치 및 색상 변경,
    form 태그의 위치 변경,
    email input태그에 EmailIcon 추가 및 스타일 변경,
    password input태그에 PasswordIcon 추가 및 스타일 변경,
    로그인 버튼 스타일 변경,
    회원가입 하이퍼링크 추가,
    비밀번호 변경(임시) 하이퍼링크 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,14 @@ module.exports = {
   theme: {
     extend: {
       screens: {
-        'xmd': '991px',
+        sm: { min: "390px", max: "819px" },
+        md: { min: "820px", max: "1023px" },
+        lg: { min: "1080px" },
       },
       colors: {
         'blue-custom': '#1A6FB3',
+        'blue-custom-deep': '#124FB6',
+        'blue-custom-hover': '#103F8D'
       },
     },
   },
