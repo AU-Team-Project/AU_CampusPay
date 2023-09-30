8c9fe514c984712ee03993c8872868d237791d00
heevic <dbgmltmd0317@gmail.com>
2023-09-30 14:58:46 +0900

    style: Register Page Design
    MobileIcon.tsx(src/components/ui/icons/MobileIcon.tsx) 아이콘 생성
    StudentIcon.tsx(src/components/ui/icons/StudentIcon/tsx) 아이콘 생성
    Page.tsx(src\app\(auth)\register\page.tsx)에 반응형 스타일 추가,
    인덱스 페이지로 이동하는 로고 이미지 추가,
    '회원가입' 텍스트의 스타일 변경
    form.tsx(src/components/form.tsx) 스타일 변경,
    email input태그에 EmailIcon 추가 및 스타일 변경,
    password input태그에 PasswordIcon 추가 및 스타일 변경,
    username input 태그에 UserIcon 추가 및 스타일 변경,
    studentnumber input 태그에 StudentIcon 추가 및 스타일 변경,
    phone input 태그에 MobileIcon 추가 및 스타일 변경,
    회원가입 버튼 스타일 변경

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,6 +1,11 @@
 'use client'
 import React, {useState} from 'react';
 import {useRouter} from "next/navigation";
+import EmailIcon from "@/components/ui/icons/EmailIcon";
+import PasswordIcon from "@/components/ui/icons/PasswordIcon";
+import UserIcon from "@/components/ui/icons/UserIcon";
+import StudentIcon from "@/components/ui/icons/StudentIcon";
+import MobileIcon from "@/components/ui/icons/MobileIcon";
 
 const FormComponent = () => {
     const router = useRouter()
@@ -21,8 +26,8 @@ const FormComponent = () => {
             method="POST"
             encType="multipart/form-data"
         >
-            <div className="rounded-md shadow-sm -space-y-px">
-                <div>
+            <div className="rounded-md shadow-sm flex flex-col gap-4">
+                <div className="relative">
                     <label htmlFor="email-address" className="sr-only">
                         이메일 주소
                     </label>
@@ -32,12 +37,15 @@ const FormComponent = () => {
                         type="email"
                         autoComplete="email"
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none outline-none rounded-[10px] relative block w-full px-3 py-2 bg-gray-100 border border-gray-300 placeholder-gray-500 text-gray-900 focus:bg-gray-200 focus:ring-blue-custom focus:border-blue-custom-deep focus:z-10 focus:scale-[1.01] sm:text-sm ease-out duration-200"
                         placeholder="이메일 주소"
                         onChange={handleInput}
                     />
+                    <span className="absolute right-[10px] top-[50%] -mt-[8px] z-10">
+                        <EmailIcon/>
+                    </span>
                 </div>
-                <div>
+                <div className="relative">
                     <label htmlFor="password" className="sr-only">
                         비밀번호
                     </label>
@@ -47,12 +55,15 @@ const FormComponent = () => {
                         type="password"
                         autoComplete="current-password"
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none outline-none rounded-[10px] relative block w-full px-3 py-2 bg-gray-100 border border-gray-300 placeholder-gray-500 text-gray-900 focus:bg-gray-200 focus:ring-blue-custom focus:border-blue-custom-deep focus:z-10 focus:scale-[1.01] sm:text-sm ease-out duration-200"
                         placeholder="비밀번호"
                         onChange={handleInput}
                     />
+                    <span className="absolute right-[10px] top-[50%] -mt-[8px] z-10">
+                        <PasswordIcon/>
+                    </span>
                 </div>
-                <div>
+                <div className="relative">
                     <label htmlFor="password" className="sr-only">
                         이름
                     </label>
@@ -62,12 +73,15 @@ const FormComponent = () => {
                         type="text"
                         autoComplete="current-password"
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none outline-none rounded-[10px] relative block w-full px-3 py-2 bg-gray-100 border border-gray-300 placeholder-gray-500 text-gray-900 focus:bg-gray-200 focus:ring-blue-custom focus:border-blue-custom-deep focus:z-10 focus:scale-[1.01] sm:text-sm ease-out duration-200"
                         placeholder="이름"
                         onChange={handleInput}
                     />
+                    <span className="absolute right-[10px] top-[50%] -mt-[8px] z-10">
+                        <UserIcon/>
+                    </span>
                 </div>
-                <div>
+                <div className="relative">
                     <label htmlFor="password" className="sr-only">
                         학번
                     </label>
@@ -77,12 +91,15 @@ const FormComponent = () => {
                         type="text"
                         autoComplete="current-password"
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none outline-none rounded-[10px] relative block w-full px-3 py-2 bg-gray-100 border border-gray-300 placeholder-gray-500 text-gray-900 focus:bg-gray-200 focus:ring-blue-custom focus:border-blue-custom-deep focus:z-10 focus:scale-[1.01] sm:text-sm ease-out duration-200"
                         placeholder="학번"
                         onChange={handleInput}
                     />
+                    <span className="absolute right-[10px] top-[50%] -mt-[8px] z-10">
+                        <StudentIcon/>
+                    </span>
                 </div>
-                <div>
+                <div className="relative">
                     <label htmlFor="phone" className="sr-only">
                         전화번호
                     </label>
@@ -92,15 +109,18 @@ const FormComponent = () => {
                         type="text"
                         autoComplete="current-password"
                         required
-                        className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                        className="appearance-none outline-none rounded-[10px] relative block w-full px-3 py-2 bg-gray-100 border border-gray-300 placeholder-gray-500 text-gray-900 focus:bg-gray-200 focus:ring-blue-custom focus:border-blue-custom-deep focus:z-10 focus:scale-[1.01] sm:text-sm ease-out duration-200"
                         placeholder="휴대폰 번호"
                         onChange={handleInput}
                     />
+                    <span className="absolute right-[10px] top-[50%] -mt-[8px] z-10">
+                        <MobileIcon/>
+                    </span>
                 </div>
             </div>
             <button
                 type="submit"
-                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                className="group relative w-full flex justify-center py-3 px-4 border border-blue-custom-hover border-transparent text-[18px] font-medium rounded-[25px] text-white bg-blue-custom-deep hover:bg-blue-custom-hover active:outline-none active:ring active:ring-offset-3 active:ring-blue-custom-hover ease-out duration-200"
                 onClick={handleClickRoute}
             >
                 회원가입
