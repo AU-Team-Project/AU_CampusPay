aefb65d498632a9e008eff92682d7ec389924d81
heevic <dbgmltmd0317@gmail.com>
2023-10-10 18:02:53 +0900

    style: Revert Admin side Bar
    layout.tsx(src\app\admin\layout.tsx)에서 반응형 스타일 제거, margin 제거
    Sidebar.tsx(src\components\admin)에서 사이드바 여닫는 기능 제거, 반응형 스타일 제거

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({children}: {children: React.ReactNode}) => {
     return (
         <div className='flex bg-gray-50'>
             <Sidebar/>
-            <main className="flex-1 w-full md:ml-20 mt-[80px] md:mb-0 mb-[110px] mx-0">
+            <main className="w-full mx-0">
                 {children}
             </main>
         </div>
