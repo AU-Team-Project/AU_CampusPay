0b56fd3ea643750cfce28081b4046ec1af7a6667
teauk03 <kto031124@gamil.com>
2023-10-11 13:57:33 +0900

    FIX - 사용자 관리 페이지에 전체 사용자의 정보가 출력되게 수정

--- src/app/admin/customers/page.tsx
+++ src/app/admin/customers/page.tsx
@@ -1,46 +1,31 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import UserIcon from "@/components/ui/icons/UserIcon";
-import {BsThreeDotsVertical} from "react-icons/bs";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { ObjectId } from "mongodb";
 
-const test = [
-    {
-        id: 1,
-        name: {
-            first: 'test',
-            last: 'last',
-        },
-        price: 5000,
-        status: 'Processing',
-        method: 'KaKao Pay',
-        date: '2023.01.01'
-    },
-    {
-        id: 2,
-        name: {
-            first: 'test',
-            last: 'last',
-        },
-        price: 5000,
-        status: 'Processing',
-        method: 'KaKao Pay',
-        date: '2023.01.01'
-    },
-    {
-        id: 3,
-        name: {
-            first: 'test',
-            last: 'last',
-        },
-        price: 5000,
-        status: 'Processing',
-        method: 'KaKao Pay',
-        date: '2023.01.01'
-    }
-]
+type UserData = {
+    _id: ObjectId,
+    email: string,
+    username: string,
+    student_number: string,
+    phone: string,
+    role: string,
+    lastAccess: string;
+}
 
-const CustomerPage = async () => {
-    const res = await fetch(`${process.env.SITE_URL}/api/admin/stats/daily`);
-    const data = await res.json();
+const CustomerPage = () => {
+    const [userData, setUserData] = useState<UserData[]>([]);
+
+    useEffect(() => {
+        fetch('/api/user') 
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data); 
+                setUserData(data.users);
+            })
+            .catch((error) => console.error('Error:', error));
+    }, []);
 
     return (
         <div className='bg-gray-100 min-h-screen'>
@@ -49,22 +34,22 @@ const CustomerPage = async () => {
                     <div className='my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
                         <span>Name</span>
                         <span className='sm:text-left text-right'>Email</span>
-                        <span className='hidden md:grid'>Last Order</span>
-                        <span className='hidden sm:grid'>Method</span>
+                        <span className='hidden md:grid'>Last Access</span>
+                        <span className='hidden sm:grid'>Role</span>
                     </div>
                     <ul>
-                        {test.map((order, id) => (
+                        {userData.map((user, id) => (
                             <li key={id} className='bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer'>
                                 <div className='flex items-center'>
                                     <div className='bg-purple-100 p-3 rounded-lg'>
-                                        <UserIcon/>
+                                        <UserIcon />
                                     </div>
-                                    <p className='pl-4'>{order.name.first + ' ' + order.name.last}</p>
+                                    <p className='pl-4'>{user.username}</p>
                                 </div>
-                                <p className='text-gray-600 sm:text-left text-right'>{order.name.first}@gmail.com</p>
-                                <p className='hidden md:flex'>{order.date}</p>
+                                <p className='text-gray-600 sm:text-left text-right'>{user.email}</p>
+                                <p className='hidden md:flex'>{user.lastAccess}</p>
                                 <div className='sm:flex hidden justify-between items-center'>
-                                    <p>{order.method}</p>
+                                    <p>{user.role}</p>
                                     <BsThreeDotsVertical />
                                 </div>
                             </li>
@@ -77,3 +62,4 @@ const CustomerPage = async () => {
 };
 
 export default CustomerPage;
+
