e57caf56174882943996e265b1fc006b4fcd5803
JangHwanPark <wkd5878@gmail.com>
2023-11-08 00:43:51 +0900

    Refactoring - 로그인 폼 입력값 상태 관리를 위한 onChange 핸들러 추가
    - 이메일과 비밀번호 필드를 위한 useState 훅 구현
    - 상태 업데이트를 위해 FormInput 컴포넌트에 onChange 프롭 추가
    - 필드 존재 여부 및 내용 형식을 확인하는 폼 유효성 검사 기능 보완
    
    CL : 243

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,64 +1,86 @@
 'use client'
-import React, {useState} from 'react';
-import {signIn} from "next-auth/react";
+import React, {useCallback, useState} from 'react';
 import Link from "next/link";
-import Image from "next/image";
-import EmailIcon from "@/components/ui/Icons/EmailIcon";
-import PasswordIcon from "@/components/ui/Icons/PasswordIcon";
+
+import {signIn} from "next-auth/react";
 import {isValidEmail, isValidPassword} from '@/service/auth';
 import {useRouter} from "next/navigation";
+
+import EmailIcon from "@/components/ui/Icons/EmailIcon";
+import PasswordIcon from "@/components/ui/Icons/PasswordIcon";
 import FormInput from "@/components/ui/Input/FormInput";
 import SmallNavbar from "@/components/nav/SmallNavbar";
 
 const LoginPage = () => {
     const router = useRouter();
-    const [emailError, setEmailError] = useState(''); // 아이디 에러 메시지 상태 추가
-    const [passwordError, setPasswordError] = useState(''); // 비밀번호 에러 메시지 상태 추가
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        const email = e.target.elements.email.value;
-        const password = e.target.elements.password.value;
+    const validateForm = () => {
+        // 오류 메세지 재설정
+        let isFormValid = true;
+        setEmailError('');
+        setPasswordError('');
 
-
-        // 이메일 유효성 검사
-        if (!isValidEmail(email)) {
+        // 이메일 공백 & 유효성 검사
+        if (!email) {
+            setEmailError('이메일 주소를 입력해주세요.');
+            isFormValid = false;
+        } else if (!isValidEmail(email)) {
             setEmailError('유효한 이메일 주소를 입력하세요.');
-            return;
-        } else {
-            setEmailError('');
+            isFormValid = false;
         }
 
-        // 비밀번호 유효성 검사
-        if (!isValidPassword(password)) {
+        // 비밀번호 공백 & 유효성 검사
+        if (!password) {
+            setPasswordError('비밀번호를 입력해주세요.');
+            isFormValid = false;
+        } else if (!isValidPassword(password)) {
             setPasswordError('비밀번호는 8-20자 길이여야 하며, 영문, 숫자, 특수문자를 포함해야 합니다.');
+            isFormValid = false;
+        }
+
+        return isFormValid;
+    }
+
+    const handleSubmit = useCallback(async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (!validateForm()) {
             return;
-        } else {
-            setPasswordError('');
         }
 
-        // 로그인 시도
-        const response = await signIn('Credentials', {
-            email,
-            password,
-            redirect: false,
-        });
+        try {
+            setIsSubmitting(true);
+            // 로그인 시도
+            const response = await signIn('Credentials', {
+                redirect: false,
+                email,
+                password,
+            });
 
-        // 로그인 결과 처리
-        if (response?.error) {
-            setEmailError('로그인에 실패했습니다. 다시 시도해주세요.');
-        } else {
-            router.replace('/');
+            // 로그인 결과 처리
+            if (response?.error) {
+                setPasswordError('로그인에 실패했습니다. 다시 시도해주세요.');
+            } else {
+                router.replace('/');
+            }
+        } catch (error) {
+            setEmailError('로그인 중 오류가 발생했습니다.');
+        } finally {
+            setIsSubmitting(false);
         }
-    };
+    }, [email, password, router]);
 
     return (
-        <div className={'overflow-x-hidden'}>
+        <div className={'flex flex-col h-screen overflow-x-hidden'}>
             {/* Header */}
             <SmallNavbar/>
             {/* Main */}
-            <div className="flex items-center justify-center pb-12 px-4 sm:px-6 lg:px-8">
-                <div className="max-w-max w-full my-36 py-20 space-y-8 p-6 bg-white rounded-md shadow-md">
+            <div className="flex-grow flex items-center justify-center pb-12 px-4 sm:px-6 lg:px-8">
+                <div className="max-w-max w-full my-24 py-20 space-y-8 p-6 bg-white rounded-md shadow-md">
                     <div className="mb-16 px-10 flex flex-col justify-center">
                         <h1 className={'text-2xl'}>
                             <span className={'font-semibold'}>CampusPay</span>에 오신 것을 환영합니다.
@@ -71,6 +93,7 @@ const LoginPage = () => {
                     <form
                         className="mt-5 space-y-6"
                         onSubmit={handleSubmit}
+                        noValidate
                     >
                         <div className="rounded-md shadow-sm flex flex-col gap-4">
                             <FormInput
@@ -80,6 +103,8 @@ const LoginPage = () => {
                                 required
                                 placeholder="이메일 주소"
                                 icon={<EmailIcon/>}
+                                value={email}
+                                onChange={(e) => setEmail(e.target.value)}
                                 errorMessage={emailError}
                             />
                             <FormInput
@@ -89,6 +114,8 @@ const LoginPage = () => {
                                 required
                                 placeholder="비밀번호"
                                 icon={<PasswordIcon/>}
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
                                 errorMessage={passwordError}
                             />
                         </div>
@@ -96,7 +123,7 @@ const LoginPage = () => {
                         <div>
                             <button
                                 type="submit"
-                                className="group relative w-full flex justify-center py-3 px-4 border border-blue-custom-hover border-transparent text-[18px] font-medium rounded-[25px] text-white bg-footer-color hover:bg-blue-custom-hover active:outline-none active:ring active:ring-offset-3 active:ring-blue-custom-hover ease-out duration-200"
+                                className={`${isSubmitting ? 'bg-gray-300' : 'bg-footer-color'} "group relative w-full flex justify-center py-3 px-4 border border-blue-custom-hover border-transparent text-[18px] font-medium rounded-[25px] text-white bg-footer-color hover:bg-blue-custom-hover active:outline-none active:ring active:ring-offset-3 active:ring-blue-custom-hover ease-out duration-200"`}
                             >로그인
                             </button>
                             <div className="mt-5 flex justify-around text-[12px] text-gray-600">
