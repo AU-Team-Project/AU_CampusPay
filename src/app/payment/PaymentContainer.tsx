2d6a7c442bc3299b638d9d0ee7a7416707b046ce
japark <wkd5878@gmail.com>
2023-11-14 13:36:21 +0900

    Merge - 디자인 변경 내역 적용
    yooheeseung -> main

--- src/app/payment/PaymentContainer.tsx
+++ src/app/payment/PaymentContainer.tsx
@@ -2,7 +2,10 @@
 import React, {useState} from 'react';
 import PaymentBtn from "@/components/ui/Button/PaymentBtn";
 import XMarkIcon from "@/components/ui/Icons/XmarkIcon";
+import PlusIcon from "@/components/ui/Icons/PlusIcon";
+import MinusIcon from "@/components/ui/Icons/MinusIcon";
 import Link from "next/link";
+import Image from "next/image";
 
 // activeTab 문자열 타입
 type activeTab = '학생' | '교직원' | '기숙사';
@@ -60,56 +63,104 @@ const PaymentContainer = () => {
         setSelectedItem(null);
     }
 
+    // + 버튼 클릭 이벤트 핸들러
+    const handleIncreaseQuantity = () => {
+        if (selectedItem && selectedItem.quantity < 999) {
+            setSelectedItem({
+                ...selectedItem,
+                quantity: selectedItem.quantity + 1
+            });
+        }
+    };
+
+    // - 버튼 클릭 이벤트 핸들러
+    const handleDecreaseQuantity = () => {
+        if (selectedItem && selectedItem.quantity > 1) {
+            setSelectedItem({
+                ...selectedItem,
+                quantity: selectedItem.quantity - 1
+            });
+        }
+    };
+
     return (
         <>
             <div className="col-span-2 flex flex-col items-center">
-                <div className="w-[900px] h-[70px] flex border-b mb-[35px] text-[22px] text-white bg-[#454545] rounded-[5px]">
+                <div className="w-[600px] h-[50px] flex text-[22px] text-white bg-[#454545]">
                     <button
                         onClick={() => setActiveTab('학생')}
-                        className={`py-2 px-4 w-full text-center ${activeTab === '학생' ? 'bg-[#009223] font-bold rounded-l-[5px] rounded-r-[50px]' : ''}`}
+                        className={`py-2 px-4 w-full text-center duration-200 hover:bg-primary-color hover:bg-opacity-90 ${activeTab === '학생' ? 'bg-primary-color font-bold' : ''}`}
                     >학생
                     </button>
                     <button
                         onClick={() => setActiveTab('교직원')}
-                        className={`py-2 px-4 w-full text-center ${activeTab === '교직원' ? 'bg-[#009223] font-bold rounded-[50px]' : ''}`}
+                        className={`py-2 px-4 w-full text-center duration-200 hover:bg-primary-color hover:bg-opacity-90 ${activeTab === '교직원' ? 'bg-primary-color font-bold' : ''}`}
                     >교직원
                     </button>
                     <button
                         onClick={() => setActiveTab('기숙사')}
-                        className={`py-2 px-4 w-full text-center ${activeTab === '기숙사' ? 'bg-[#009223] font-bold rounded-l-[50px] rounded-r-[5px]' : ''}`}
+                        className={`py-2 px-4 w-full text-center duration-200 hover:bg-primary-color hover:bg-opacity-90 ${activeTab === '기숙사' ? 'bg-primary-color font-bold' : ''}`}
                     >기숙사
                     </button>
                 </div>
 
-                <div className="grid grid-cols-1">
+                <div className="flex gap-[40px] mt-[60px]">
                     {menuData[activeTab].map((item, idx) => (
-                        <div key={idx} className="flex gap-3 bg-white mb-[30px] w-[900px] h-[100px] rounded-[5px]">
-                            <div className="m-3 min-w-[460px] grid grid-cols-2 grid-rows-2 leading-[35px]">
-                                <h4 className="font-bold text-[22px]">{item.label}</h4>
-                                <p className="text-[#FFCE32] font-semibold text-right">{item.price} 원</p>
-                                <h3 className="text-[#555555]">메뉴 메뉴 메뉴 메뉴</h3> {/* 오늘의 메뉴 구성 추가 예정  */}
-                                <p className="text-right">2023.11.04</p> {/* 메뉴에 담긴 오늘의 날짜 추가 예정 */}
+                        <div key={idx} className="w-[240px] h-[350px] bg-white rounded-[6px] cursor-pointer"
+                             onClick={() => handleItemClick(item.label, item.price)}>
+                            <div className="w-full h-[240px] bg-black-color rounded-t-[6px]">
+                                <Image
+                                    className="rounded-t-[6px]"
+                                    src={'/img/TemporaryMenu.jpg'}
+                                    alt={'임시 메뉴 이미지'}
+                                    width={240}
+                                    height={240}
+                                />
                             </div>
-                            <button className="ml-[320px] m-3 w-[75px] h-[75px] bg-[#ececec] text-white font-medium text-[55px] rounded-[5px]"
-                                    onClick={() => handleItemClick(item.label, item.price)}>
-                                +
-                            </button>
+                            <div className="p-2 min-w-full leading-[35px]">
+                                <h4 className="text-[20px] text-center font-medium">{item.label}</h4>
+                                <div className="grid grid-cols-2 grid-rows-2">
+                                    {/* Active Tab의 현재 상태값 표시 및  */}
+                                    <p className="text-[16px]">{activeTab}</p>
+                                    <p></p>
+                                    <p className="text-[18px] font-medium">{item.price} 원</p>
+                                    <p className="text-right text-[16px]">2023.11.04</p>
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
             </div>
 
-            <div className='flex flex-col justify-center items-center'>
+            <div className='flex flex-col justify-center items-center pt-16 border-t-[1px]'>
                 {selectedItem ? (
-                    <ul className="relative w-[900px] h-[150px] bg-[#ececec] leading-[150px] text-center rounded-[5px]">
-                        <li key={selectedItem.label} className="my-8 mx-14">
-                            <div className="m-3 min-w-[760px] grid grid-cols-2 grid-rows-2 gap-[17px] leading-[35px]">
-                                <p className="text-[22px] text-[#555555] text-left">{selectedItem.label}</p>
-                                <p></p>
-                                <p className="text-[18px] text-[#555555] text-left">메뉴 메뉴 메뉴 메뉴</p>
-                                <p className="text-[18px] text-[#3284FF] text-right">
-                                    결제금액: {selectedItem ? selectedItem.price * selectedItem.quantity : 0} 원
-                                </p>
+                    <ul className="flex flex-col justify-center w-[800px] h-[200px] bg-[#eaeaea] rounded-[5px]">
+                        <li key={selectedItem.label} className="relative grid grid-cols-10 max-h-[120px] mx-[100px] p-[10px] bg-white">
+                            <div className="w-[100px] h-[100px] col-span-2 bg-black-color rounded-[1px]">
+                                <Image
+                                    className="rounded-t-[1px]"
+                                    src={'/img/TemporaryMenu.jpg'}
+                                    alt={'임시 메뉴 이미지'}
+                                    width={100}
+                                    height={100}
+                                />
+                            </div>
+                            <div className="p-2 min-w-full col-span-8">
+                                <h4 className="text-[20px] font-medium">{selectedItem.label}</h4>
+                                <div className="flex gap-44 mt-[10px]">
+                                    <p className="text-[18px] font-medium">{selectedItem ? selectedItem.price: 0} 원</p>
+                                    <div className="flex gap-3">
+                                        <p className="px-3 text-right text-[16px] min-w-[100px] max-w-[100px]">{selectedItem.quantity}개</p>
+                                        <button className="w-fit h-fit p-[3px] bg-white border-[1px] border-[#7c7c7c] rounded-[3px]"
+                                                onClick={handleIncreaseQuantity}>
+                                            <PlusIcon/>
+                                        </button>
+                                        <button className="w-fit h-fit p-[3px] bg-white border-[1px] border-[#7c7c7c] rounded-[3px]"
+                                                onClick={handleDecreaseQuantity}>
+                                            <MinusIcon/>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                             <button className="absolute p-5 right-0 top-0" onClick={handleItemDeselection}>
                                 <XMarkIcon/>
@@ -117,41 +168,28 @@ const PaymentContainer = () => {
                         </li>
                     </ul>
                 ): (
-                    <ul className="w-[900px] h-[150px] bg-[#ececec] text-[#555555] text-center rounded-[5px]">
+                    <ul className="w-[800px] h-[150px] bg-[#ececec] text-[#555555] text-center rounded-[5px]">
                         <li className="my-16">
                             장바구니가 비었습니다.
                         </li>
                     </ul>
                 )}
 
-                <div className="flex justify-center gap-[300px] mt-[30px]">
-                    <Link href={'/'}>
-                        <div
-                            className='
-                                w-[300px]
-                                h-[65px]
-                                bg-[#393939]
-                                font-semibold
-                                text-center
-                                leading-[65px]
-                                text-white
-                                rounded-[5px]
-                            '
-                        >
-                            돌아가기
-                        </div>
-                    </Link>
-                    <PaymentBtn
-                        props={selectedItem}
-                        className='
-                            w-[300px]
-                            h-[65px]
-                            bg-[#009223]
-                            font-semibold
-                            text-white
-                            rounded-[5px]
-                        '
-                    />
+                <div className="flex flex-col justify-center mt-[30px]">
+                    <p className="pb-10 text-center">
+                        총 상품 금액: {selectedItem ? selectedItem.price * selectedItem.quantity : 0}원
+                    </p>
+                    <div className="flex gap-[60px]">
+                        <Link href={'/'}>
+                            <div className={'bg-[#555555] font-semibold text-center leading-[65px] text-white w-[300px] h-[65px] rounded-[5px] duration-200 hover:bg-[#444444]'}>
+                                돌아가기
+                            </div>
+                        </Link>
+                        <PaymentBtn
+                            props={selectedItem}
+                            className={'bg-primary-color font-semibold text-white w-[300px] h-[65px] border-2 border-solid border-primary-color rounded-[5px] duration-200 hover:bg-[#ff8a00] hover:border-primary-color'}
+                        />
+                    </div>
                 </div>
             </div>
 
--- src/app/ticket/[slug]/page.tsx
+++ src/app/ticket/[slug]/page.tsx
@@ -2,14 +2,15 @@ import React from 'react';
 import {Menu} from "@/model/menu";
 import Link from "next/link";
 
-import {options} from "@/app/api/auth/[...nextauth]/options";
-import {ObjectId} from "mongodb";
+import TopNavbar from "@/components/nav/Navbar";
 import {getServerSession} from "next-auth";
-import {redirect} from "next/navigation";
+import {AiOutlineQrcode} from "react-icons/ai";
 
+import {options} from "@/app/api/auth/[...nextauth]/options";
+import {redirect} from "next/navigation";
+import {ObjectId} from "mongodb";
+import Image from "next/image";
 import Navbar from "@/components/nav/Navbar";
-import Button from "@/components/ui/Button/Button";
-import DesktopNavbar from "@/components/nav/DesktopNavbar";
 
 type Props = {
     params: {
@@ -28,11 +29,9 @@ const TicketPage = async ({params}: Props) => {
 
     // 현재 로그인한 사용자 이름 또는 아이디
     const currentUserId = session?.user._id || session?.user.username;
+
     // 현재 로그인한 사용자 기준으로 데이터 패칭
-    const res = await fetch(`
-        ${process.env.SITE_URL}/api/confirmation/user?name=${currentUserId}
-        `
-    )
+    const res = await fetch(`${process.env.SITE_URL}/api/confirmation/user?name=${currentUserId}`)
     const data = await res.json();
     const featDate = data.data.reverse();
 
@@ -41,96 +40,47 @@ const TicketPage = async ({params}: Props) => {
 
     return (
         <Navbar>
-            <DesktopNavbar/>
             <main className='mx-5 my-10'>
-                <h2 className="text-2xl font-bold mb-5">
-                    내 식권 목록
-                </h2>
-                {unusedItems.length > 0 ? (
-                    <>
-                        {unusedItems.map((item: Menu) => (
-                            <div
-                                key={item._id}
-                                className='
-                                    mb-5
-                                    flex
-                                    flex-col
-                                    md:flex-row
-                                    items-start
-                                    md:items-center
-                                    p-5
-                                    border
-                                    border-gray-300
-                                    rounded-lg
-                                    shadow-sm
-                                    hover:shadow-md
-                                    transition-shadow
-                                    duration-300
-                                    bg-white
-                                '
-                            >
-                                <div className="md:flex-grow">
-                                    <Link href={`/confirmation/${encodeURIComponent(item.name)}?id=${encodeURIComponent(item._id)}&menu=${encodeURIComponent(item.menu ?? '')}&state=${encodeURIComponent(item.state ?? '')}`}>
-                                        <h3
-                                            className='
-                                                text-lg
-                                                font-medium
-                                                text-blue-600
-                                                mb-2
-                                            '
-                                        >
-                                            {item.menu}
-                                        </h3>
-                                        <p className="text-gray-500">
-                                            금액: {item.amount}원
-                                        </p>
-                                        <p className="text-gray-500">
-                                            임시값: {item._id}
-                                        </p>
-                                        <p className="text-gray-500">
-                                            임시값: {item.name}
-                                        </p>
-                                    </Link>
+                <h1 className="pb-10 text-center font-medium text-[40px]">내 식권 목록</h1>
+                <div className="flex flex-col items-center w-full">
+                    {unusedItems.length > 0 ? (
+                        <>
+                            {unusedItems.map((item: Menu) => (
+                                <div key={item._id}
+                                     className='mb-5 flex flex-col w-[900px] justify-between md:flex-row items-start md:items-center p-3 border border-gray-300 rounded-lg shadow-sm hover:shadow-md transition-shadow duration-300 bg-white cursor-pointer'>
+                                    <div className="flex items-center gap-[75px]">
+                                        <div className="w-[100px] h-[100px] col-span-2 bg-black-color rounded-[1px]">
+                                            <Image
+                                                className="rounded-[6px]"
+                                                src={'/img/TemporaryMenu.jpg'}
+                                                alt={'임시 메뉴 이미지'}
+                                                width={100}
+                                                height={100}
+                                            />
+                                        </div>
+                                        <Link
+                                            href={`/confirmation/${encodeURIComponent(item.name)}?id=${encodeURIComponent(item._id)}&menu=${encodeURIComponent(item.menu ?? '')}&state=${encodeURIComponent(item.state ?? '')}`}>
+                                            <h3 className="text-[22px] font-semibold mb-[20px]">{item.menu}</h3>
+                                            <p className="text-gray-500 text-[18px]">임시값: {item._id}</p>
+                                            {/* DB 마이그레이션을 통한 식당 표시 기능 필요 */}
+                                        </Link>
+                                    </div>
+                                    <div className="flex flex-col items-end gap-3">
+                                        <div className="w-fit border-[1px] border-black">
+                                            <AiOutlineQrcode size="60px"/>
+                                        </div>
+                                        <div
+                                            className="w-fit py-[1px] px-[25px] text-[18px] text-white bg-primary-color rounded-[50px]">{item.time}</div>
+                                    </div>
                                 </div>
-                                <div className="mt-3 md:mt-0">
-                                    <Button
-                                        text='취소요청'
-                                        className='
-                                            px-4
-                                            py-2
-                                            bg-red-500
-                                            text-white
-                                            text-xs
-                                            rounded
-                                            font-medium
-                                            tracking-wide
-                                            hover:bg-red-600
-                                            transition
-                                            ease-in-out
-                                            duration-300
-                                        '
-                                    />
-                                </div>
-                            </div>
-                        ))}
-                    </>
-                ) : (
-                    <div
-                        className='
-                            py-10
-                            px-6
-                            border
-                            border-gray-300
-                            rounded-lg
-                            shadow-md
-                            text-center
-                        '
-                    >
-                        <p className="text-gray-500">
-                            사용 가능한 식권이 없습니다.
-                        </p>
-                    </div>
-                )}
+                            ))}
+                        </>
+                    ) : (
+                        <div className="py-10 px-6 border border-gray-300 rounded-lg shadow-md text-center">
+                            <p className="text-gray-500">사용 가능한 식권이 없습니다.</p>
+                        </div>
+                    )}
+                </div>
             </main>
         </Navbar>
     );
